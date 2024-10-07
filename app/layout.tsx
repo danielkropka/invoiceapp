027b886bf6bb00bc3e5685d2e18c317de8d6b4ed
danielkropka <danielwarpechowski9@gmail.com>
2024-10-07 23:14:16 +0200

    feat: preparing default files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import React from "react";
+import { cn } from "@/lib/utils";
+import Provider from "@/components/Provider";
+import { Toaster } from "sonner";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(inter.className, "flex flex-col min-h-screen w-full")}
       >
-        {children}
+        <Provider>{children}</Provider>
+        <Toaster />
       </body>
     </html>
   );
