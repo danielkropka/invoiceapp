3d662205836fb74b0d4efe20f788a2d3c7e584f8
danielkropka <danielwarpechowski9@gmail.com>
2024-10-07 23:16:46 +0200

    feat(auth): preparing auth options for next auth

---
+++ lib/auth.ts
@@ -0,0 +1,54 @@
+import { getServerSession, NextAuthOptions } from "next-auth";
+import { db } from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import { nanoid } from "nanoid";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        token.id = user.id;
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+    redirect() {
+      return "/";
+    },
+  },
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
