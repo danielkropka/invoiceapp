0208d524277a16a9da5429f2e1905a09325bab18
danielkropka <danielwarpechowski9@gmail.com>
2024-10-10 18:09:53 +0200

    fix: 504: GATEWAY_TIMEOUT

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -8,7 +8,7 @@ import { signIn } from "next-auth/react";
 export default function Page() {
   const loginWithGoogle = async () => {
     try {
-      await signIn("google");
+      await signIn("google", { callbackUrl: "/" });
     } catch (error) {
       console.log(error);
     }
