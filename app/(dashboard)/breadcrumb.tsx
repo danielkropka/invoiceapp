4c963618d7f3a8d00e7d2c1dfb1696e5c1a082e9
danielkropka <danielwarpechowski9@gmail.com>
2024-10-16 19:49:46 +0200

    feat: breadcrumb new component

---
+++ app/(dashboard)/breadcrumb.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import React from "react";
+
+export default function DashboardBreadCrumb() {
+  const paths = usePathname();
+  const pathNames = paths.split("/").filter((path) => path);
+  const convertPathNames = (path: string) => {
+    switch (path) {
+      case "clients":
+        return "Klienci";
+      case "create":
+        return "Kreator";
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Breadcrumb className="hidden md:flex">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href={"/"}>Strona główna</BreadcrumbLink>
+        </BreadcrumbItem>
+        {pathNames.length !== 0 && <BreadcrumbSeparator />}
+        {pathNames.map((path, index) => {
+          const href = pathNames.slice(0, index + 1).join("/");
+
+          return (
+            <React.Fragment key={index}>
+              <BreadcrumbLink href={`/${href}`}>
+                {convertPathNames(path)}
+              </BreadcrumbLink>
+              {pathNames.length !== index + 1 && <BreadcrumbSeparator />}
+            </React.Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
