74f0f03a46834b9c6b12127378d515030fa43438
danielkropka <danielwarpechowski9@gmail.com>
2024-10-16 19:53:04 +0200

    feat: update products component

---
+++ app/(dashboard)/create/products.tsx
@@ -0,0 +1,18 @@
+import { UseFormReturn } from "react-hook-form";
+import { invoiceType } from "./invoice-creator-form";
+
+export default function Products({
+  form,
+}: {
+  form: UseFormReturn<invoiceType, any, undefined>;
+}) {
+  /* const {
+    fields: products,
+    append,
+    remove,
+  } = useFieldArray({
+    control: form.control,
+    name: "products",
+  }); */
+  return <span>{form.watch("client.email")}</span>;
+}
