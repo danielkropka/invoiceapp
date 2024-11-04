90536172bd037f4ab5aa483534fd3428484b6dd6
danielkropka <danielwarpechowski9@gmail.com>
2024-11-04 21:31:49 +0100

    feat: confirm invoice

--- app/confirm-invoice/confirmInvoice.tsx
+++ app/confirm-invoice/confirmInvoice.tsx
@@ -1,5 +1,34 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function ConfirmInvoice() {
-  return <Button>Potwierdź opłacenie faktury</Button>;
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useTransition } from "react";
+import { toast } from "sonner";
+
+export default function ConfirmInvoice({ token }: { token: string }) {
+  const [isPending, startTrasition] = useTransition();
+
+  const handleConfirm = () => {
+    startTrasition(async () => {
+      try {
+        await axios.post("/api/invoice/confirm", {
+          token,
+        });
+
+        toast.info(
+          "Potwierdzenie zostało przesłane do wystawcy faktury. Poinformujemy Cię drogą mailową, gdy opłacenie faktury zostanie zweryfikowane przez wystawcę."
+        );
+      } catch (err) {
+        toast.error(
+          "Wystąpił błąd w trakcie wysyłania potwierdzenia do wystawcy faktury. Spróbuj ponownie później."
+        );
+      }
+    });
+  };
+
+  return (
+    <Button onClick={handleConfirm} isLoading={isPending}>
+      Potwierdź opłacenie faktury
+    </Button>
+  );
 }
--- app/confirm-invoice/page.tsx
+++ app/confirm-invoice/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ConfirmInvoice from "./confirmInvoice";
 
 interface PageProps {
@@ -11,27 +12,38 @@ export default function ConfirmInvoicePage({ searchParams }: PageProps) {
   const toEmail = searchParams.to;
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      {token && typeof token === "string" ? (
-        <ConfirmInvoice />
-      ) : (
-        <div className="flex h-full flex-col items-center justify-center space-y-1">
-          <h3 className="font-semibold text-2xl">
-            Sprawdź swoją skrzynkę pocztową
-          </h3>
+    <div className="flex flex-col items-center justify-between h-screen gap-10 bg-gradient-to-br from-slate-100 dark:from-slate-900">
+      <h2 className="font-semibold text-7xl tracking-tight text-[#0056b3] mt-4">
+        Fakturly
+      </h2>
+      <div className="flex flex-col justify-center items-center h-full gap-4">
+        <Image
+          src={"/confirm.svg"}
+          alt="confirm image"
+          width={300}
+          height={300}
+        />
+        {token && typeof token === "string" ? (
+          <ConfirmInvoice token={token} />
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <h3 className="font-semibold text-2xl">
+              Sprawdź swoją skrzynkę pocztową
+            </h3>
 
-          {toEmail ? (
-            <p className="text-muted-foreground text-center">
-              Twoją fakturę wysłaliśmy na podany email{" "}
-              <span className="font-semibold">{toEmail}</span>.
-            </p>
-          ) : (
-            <p className="text-muted-foreground text-center">
-              Wysłaliśmy fakturę na twojego e-maila.
-            </p>
-          )}
-        </div>
-      )}
+            {toEmail ? (
+              <p className="text-muted-foreground text-center">
+                Twoją fakturę wysłaliśmy na podany email{" "}
+                <span className="font-semibold">{toEmail}</span>.
+              </p>
+            ) : (
+              <p className="text-muted-foreground text-center">
+                Wysłaliśmy fakturę na twojego e-maila.
+              </p>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
